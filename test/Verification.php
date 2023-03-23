829012cd6f842440a4ad78bd24b59756f3073b09
Rado <mickaelradosetraraveloson@gmail.com>
2023-03-23 11:52:41 +0300

    Verification post

---
+++ test/Verification.php
@@ -0,0 +1,14 @@
+<?php
+// Verifier si la variable $_POST ou $_GET sont definit: contient des valeurs
+// 
+function verificate($globalVariable,$variables){
+    $isDefine = false;
+    foreach($variables as $elem){
+        if(isset($globalVariable[$elem])){
+            $isDefine = true;
+        }
+    } 
+    return $isDefine;
+}
+
+?>
