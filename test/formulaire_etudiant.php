c1d72ce0f8ef2ac36699b9656b47089edce22636
Donovan700 <bryanrakotosedson@gmail.com>
2023-03-23 10:46:50 +0300

    Utilisation du require connexion

--- test/formulaire_etudiant.php
+++ test/formulaire_etudiant.php
@@ -8,23 +8,9 @@
 </head>
 <body>
 
+<?php require("Connexion.php"); ?>
 
 <?php
-
-$host = 'localhost';
-$dbname = 'soutenances';
-$user = 'root';
-$password = '';
-/* Connexion à la base de données avec PDO */
-try {
-	$bdd = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-	$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "connected with the database ". $dbname ;
-    } 
-catch (PDOException $e) {
-    echo "Erreur : " . $e->getMessage();
-    die();
-    }
 /* Class etudiant */
 class Etudiant {
     private $matricule;
